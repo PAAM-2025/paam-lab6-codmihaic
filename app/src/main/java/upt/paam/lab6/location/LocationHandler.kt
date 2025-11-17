7169b2f0e377b7a3f53ee0c9b082d3f85df17350
codmihaic <codreanumihaic@gmail.com>
2025-11-17 10:50:02 +0200

    Update LocationHandler.kt
    LocationHandler complet

--- app/src/main/java/upt/paam/lab6/location/LocationHandler.kt
+++ app/src/main/java/upt/paam/lab6/location/LocationHandler.kt
@@ -8,6 +8,8 @@ import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
+import android.os.Looper
+
 
 @SuppressLint("MissingPermission")
 class LocationHandler(context: Context) {
@@ -18,9 +20,19 @@ class LocationHandler(context: Context) {
     fun registerLocationListener(locationCallback: LocationCallback) {
         // TODO 2: Create a LocationRequest with Priority.PRIORITY_HIGH_ACCURACY,
         //  min update distance set to 5m and update interval to 100ms.
+        val locationRequest = LocationRequest.Builder(
+            Priority.PRIORITY_HIGH_ACCURACY,
+            /* intervalMillis = */ 100L
+        )
+            .setMinUpdateDistanceMeters(5f)
+            .build()
 
         // TODO 3: Register request and callback with the fused location service client.
-
+        client.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
     }
 
     fun unregisterLocationListener(locationCallback: LocationCallback) {
